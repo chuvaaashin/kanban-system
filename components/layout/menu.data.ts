6bc766422559e59d7fb5031b7b1bdb5176c5349e
Nikita <chuvaaash@gmail.com>
2025-04-27 22:52:09 +0300

    edit menu list

--- components/layout/menu.data.ts
+++ components/layout/menu.data.ts
@@ -11,14 +11,9 @@ export const MENU_DATA: IMenuItem[] = [
         url: '/',
     },
     {
-        name: 'Продукты',
-        icon: 'ep:goods',
-        url: '/products',
-    },
-    {
-        name: 'Платежи',
-        icon: 'ph:contactless-payment',
-        url: '/payments',
+        icon: 'material-symbols:view-kanban',
+        name: 'Канбан',
+        url: '/',
     },
     {
         name: 'Заказы',
@@ -29,20 +24,5 @@ export const MENU_DATA: IMenuItem[] = [
         name: 'Клиенты',
         icon: 'mingcute:group-line',
         url: '/customers',
-    },
-    {
-        name: 'Отзывы',
-        icon: 'fluent:person-feedback-48-regular',
-        url: '/feedback',
-    },
-    {
-        name: 'Настройки',
-        icon: 'radix-icons:gear',
-        url: '/settings',
-    },
-    {
-        name: 'Поддержка',
-        icon: 'radix-icons:question-mark',
-        url: '/help',
     },
 ]
